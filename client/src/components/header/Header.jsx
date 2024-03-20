a8969d4883a824908e01cc2ebc935234cddc9fd6
FaizShaikh0705 <faizshaikh0705@gmail.com>
2024-03-20 15:06:59 +0530

    marksheet

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -74,50 +74,52 @@ const Header = () => {
                 <div className="rowh-100 text-center">
                     <h1 style={{ paddingTop: "400px", fontSize: "32px" }}>Online Fake Document Detection System</h1>
                 </div>
-                <Tabs>
-                    <TabList>
-                        <Tab>PanCard</Tab>
-                        <Tab>AadharCard</Tab>
-                        <Tab>10th Marksheet</Tab>
-                    </TabList>
+                <div className='panel'>
+                    <Tabs>
+                        <TabList>
+                            <Tab>PanCard</Tab>
+                            <Tab>AadharCard</Tab>
+                            <Tab>10th Marksheet</Tab>
+                        </TabList>
 
-                    <TabPanel>
-                        <div>
-                            <input type="file" accept="image/*" onChange={handleImageChange} />
-                            <button onClick={handleImageUpload}>Upload Image</button>
-                            {response && (
-                                <div>
-                                    <h2>Response:</h2>
-                                    <p>{response}</p>
-                                </div>
-                            )}
-                        </div>
-                    </TabPanel>
-                    <TabPanel>
-                        <div>
-                            <input type="file" accept="image/*" onChange={handleImageChange} />
-                            <button onClick={handleImageUpload1}>Upload Image</button>
-                            {response && (
-                                <div>
-                                    <h2>Response:</h2>
-                                    <p>{response}</p>
-                                </div>
-                            )}
-                        </div>
-                    </TabPanel>
-                    <TabPanel>
-                        <div>
-                            <input type="file" accept="image/*" onChange={handleImageChange} />
-                            <button onClick={handleImageUpload2}>Upload Image</button>
-                            {response && (
-                                <div>
-                                    <h2>Response:</h2>
-                                    <p>{response}</p>
-                                </div>
-                            )}
-                        </div>
-                    </TabPanel>
-                </Tabs>
+                        <TabPanel>
+                            <div className='panel'>
+                                <input type="file" accept="image/*" onChange={handleImageChange} />
+                                <button onClick={handleImageUpload}>Upload Image</button>
+                                {response && (
+                                    <div>
+                                        <h2>Response:</h2>
+                                        <p>{response}</p>
+                                    </div>
+                                )}
+                            </div>
+                        </TabPanel>
+                        <TabPanel>
+                            <div className='panel'>
+                                <input type="file" accept="image/*" onChange={handleImageChange} />
+                                <button onClick={handleImageUpload1}>Upload Image</button>
+                                {response && (
+                                    <div>
+                                        <h2>Response:</h2>
+                                        <p>{response}</p>
+                                    </div>
+                                )}
+                            </div>
+                        </TabPanel>
+                        <TabPanel>
+                            <div className='panel'>
+                                <input type="file" accept="image/*" onChange={handleImageChange} />
+                                <button onClick={handleImageUpload2}>Upload Image</button>
+                                {response && (
+                                    <div>
+                                        <h2>Response:</h2>
+                                        <p>{response}</p>
+                                    </div>
+                                )}
+                            </div>
+                        </TabPanel>
+                    </Tabs>
+                </div>
             </div>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320"><path fill="#fff" fill-opacity="1" d="M0,160L48,133.3C96,107,192,53,288,74.7C384,96,480,192,576,213.3C672,235,768,181,864,160C960,139,1056,149,1152,170.7C1248,192,1344,224,1392,240L1440,256L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"></path></svg>
         </section>
